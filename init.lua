2527eed300d6b9ff5d9d20fce373e2480970c04f
Gergo Ignacz <ignaczg@login510-27.ibex.kaust.edu.sa>
2026-03-18 13:51:42 +0300

    updated keybindings of $ and _ for vim

--- init.lua
+++ init.lua
@@ -13,5 +13,21 @@ vim.g.clipboard = {
   },
 }
 
+-- Move line down with Option+Down
+vim.keymap.set('n', '<A-Down>', ':m .+1<CR>==', { noremap = true, silent = true })
+-- Move line up with Option+Up
+vim.keymap.set('n', '<A-Up>', ':m .-2<CR>==', { noremap = true, silent = true })
+
+-- Visual mode: move selection down/up
+vim.keymap.set('v', '<A-Down>', ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
+vim.keymap.set('v', '<A-Up>', ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
+
+-- Remap of _ to jump to the end of the line instead of beginning [this was $ before]
+vim.keymap.set({'n', 'v', 'o'}, '_', '$', {noremap = true, silent = true, desc = "Jump to the end of the line"})
+
+
+-- Remap of _ to jump to the end of the line instead of beginning [this was $ before]
+vim.keymap.set({'n', 'v', 'o'}, '$', '_', {noremap = true, silent = true, desc = "Jump to the beginning of the line"})
+
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_ruby_provider = 0
